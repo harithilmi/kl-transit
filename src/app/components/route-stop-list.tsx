174da85226cec6facbf2b3d0ed59459cd0be0625
Harith <harithpopular@gmail.com>
2024-11-15 12:33:45 +0800

    Add arrow to line

--- src/app/components/route-stop-list.tsx
+++ src/app/components/route-stop-list.tsx
@@ -9,8 +9,12 @@ export function RouteStopList({ services }: RouteStopListProps) {
   const sortedServices = [...services].sort((a, b) => a.sequence - b.sequence)
 
   // Get services for each direction
-  const direction1Services = sortedServices.filter(service => service.direction === 1)
-  const direction2Services = sortedServices.filter(service => service.direction === 2)
+  const direction1Services = sortedServices.filter(
+    (service) => service.direction === 1,
+  )
+  const direction2Services = sortedServices.filter(
+    (service) => service.direction === 2,
+  )
 
   // Get first and last stops for each direction
   const direction1First = direction1Services[0]
@@ -25,23 +29,23 @@ export function RouteStopList({ services }: RouteStopListProps) {
   console.log('Direction 1:', {
     first: {
       stop_id: direction1First?.stop_id,
-      stop_name: direction1First?.stop?.stop_name
+      stop_name: direction1First?.stop?.stop_name,
     },
     last: {
       stop_id: direction1Last?.stop_id,
-      stop_name: direction1Last?.stop?.stop_name
-    }
+      stop_name: direction1Last?.stop?.stop_name,
+    },
   })
 
   console.log('Direction 2:', {
     first: {
       stop_id: direction2First?.stop_id,
-      stop_name: direction2First?.stop?.stop_name
+      stop_name: direction2First?.stop?.stop_name,
     },
     last: {
       stop_id: direction2Last?.stop_id,
-      stop_name: direction2Last?.stop?.stop_name
-    }
+      stop_name: direction2Last?.stop?.stop_name,
+    },
   })
 
   return (
@@ -70,10 +74,12 @@ export function RouteStopList({ services }: RouteStopListProps) {
           <div className="flex-1 flex flex-col h-full justify-between">
             {direction1Services.map(
               (service) =>
-                ((!isSharedFirstStop || service.stop_id !== direction1First?.stop_id) &&
-                (!isSharedLastStop || service.stop_id !== direction1Last?.stop_id)) && (
+                (!isSharedFirstStop ||
+                  service.stop_id !== direction1First?.stop_id) &&
+                (!isSharedLastStop ||
+                  service.stop_id !== direction1Last?.stop_id) && (
                   <div
-                    className="hover:bg-white/10 px-6 rounded-lg transition-colors duration-300 pr-10 py-4 relative after:absolute after:top-0 after:right-0 after:w-1 after:h-full after:bg-indigo-600 after:translate-x-[-1.25rem] grow min-h-0"
+                    className="hover:bg-white/10 px-6 rounded-lg transition-colors duration-300 pr-10 py-4 relative after:absolute after:top-0 after:right-0 after:w-1 after:h-full after:bg-indigo-600 after:translate-x-[-1.25rem] grow min-h-0 before:absolute before:top-1/2 before:right-0 before:w-2 before:h-2 before:border-t-2 before:border-r-2 before:border-white before:z-10 before:translate-x-[-1.125rem] before:-translate-y-1/2 before:rotate-[135deg] flex flex-col justify-center"
                     key={service.stop_id}
                   >
                     <h3 className="text-sm text-gray-400 text-right">
@@ -93,10 +99,12 @@ export function RouteStopList({ services }: RouteStopListProps) {
           <div className="flex-1 flex flex-col h-full justify-between">
             {[...direction2Services].reverse().map(
               (service) =>
-                ((!isSharedFirstStop || service.stop_id !== direction2Last?.stop_id) &&
-                (!isSharedLastStop || service.stop_id !== direction2First?.stop_id)) && (
+                (!isSharedFirstStop ||
+                  service.stop_id !== direction2Last?.stop_id) &&
+                (!isSharedLastStop ||
+                  service.stop_id !== direction2First?.stop_id) && (
                   <div
-                    className="hover:bg-white/10 px-6 rounded-lg transition-colors duration-300 pl-10 py-4 relative after:absolute after:top-0 after:left-0 after:w-1 after:h-full after:bg-indigo-600 after:translate-x-[1.25rem] grow"
+                    className="hover:bg-white/10 px-6 rounded-lg transition-colors duration-300 pl-10 py-4 relative after:absolute after:top-0 after:left-0 after:w-1 after:h-full after:bg-indigo-600 after:translate-x-[1.25rem] grow before:absolute before:top-1/2 before:left-0 before:w-2 before:h-2 before:border-t-2 before:border-r-2 before:border-white before:z-10 before:translate-x-[1.125rem] before:rotate-[-45deg] flex flex-col justify-center"
                     key={service.stop_id}
                   >
                     <h3 className="text-sm text-gray-400 text-left">
