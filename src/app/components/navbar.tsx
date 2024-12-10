1e781e90b84eb2f4bab82d5b31012f50aa180cef
Harith <harithpopular@gmail.com>
2024-12-11 03:46:05 +0800

    Made the navbar clickable to home

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { ThemeToggle } from './theme-toggle'
 import { LanguageSwitcher } from './language-switcher'
 import { SignedOut, SignedIn, SignInButton, UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
 export function Navbar() {
   return (
     <div className="flex items-center justify-between p-4 bg-background text-foreground">
-      <div className="text-lg font-bold">KL Transit</div>
+      <Link href="/" className="text-lg font-bold hover:opacity-80">
+        KL Transit
+      </Link>
       <div className="flex items-center gap-4">
         <ThemeToggle />
         <LanguageSwitcher />
